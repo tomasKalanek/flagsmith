3cf2653c33338131a6bfa1cd0286628a99edc8e6
Tomáš <tomas_kalanek@kb.cz>
2022-02-14 13:42:55 +0100

    Test

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Script from 'next/script'
 import {useState} from 'react';
 export default function Home() {
     console.log('test');
-    console.log(process.env.pusherkey);
+    console.log(process.env.PUSHERKEY);
   const [isReady, setReady] = useState(false)
   return (
     <div className={styles.container}>
